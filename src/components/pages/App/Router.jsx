8970f11188e6318cee1a383b80e09464d6b7342c
suzukalight <masahiko_kubara@relationsgroup.co.jp>
2019-03-29 00:18:59 +0900

    Use query in post page

--- src/components/pages/App/Router.jsx
+++ src/components/pages/App/Router.jsx
@@ -3,77 +3,15 @@ import PropTypes from "prop-types";
 import { Switch, Route } from "react-router-dom";
 
 import App from "./App";
-import Post from "../posts";
+import Post from "../posts/detail";
 import Root from "../root";
-
-const content = `
-Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
-ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et
-magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
-ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
-quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget,
-arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
-Nullam dictum felis eu pede link mollis pretium. Integer tincidunt. Cras
-dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.
-Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.
-Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus
-viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet.
-Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.
-
-
-Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
-ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et
-magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
-ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
-quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget,
-arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
-Nullam dictum felis eu pede link mollis pretium. Integer tincidunt. Cras
-dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.
-Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.
-Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus
-viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet.
-Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.`;
-
-const posts = [
-  {
-    id: 1,
-    title: "Lorem ipsum dolor sit amet",
-    content
-  },
-  {
-    id: 2,
-    title: "consectetuer adipiscing elit",
-    content
-  },
-  {
-    id: 3,
-    title: "Aenean commodo ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et magnis dis parturient montes",
-    content
-  }
-];
-
-const comments = [
-  {
-    id: 1,
-    content:
-      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis"
-  },
-  {
-    id: 2,
-    content:
-      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis"
-  }
-];
 
 export default function AppRouter({ history, location }) {
   return (
     <App>
       <Switch>
-        <Route
-          path="/posts"
-          render={() => <Post post={posts[0]} comments={comments} />}
-        />
-        <Route path="/" render={() => <Root posts={posts} />} />
+        <Route path="/posts/:id" render={() => <Post />} />
+        <Route path="/" render={() => <Root />} />
       </Switch>
     </App>
   );
---
+++ src/components/pages/posts/detail/index.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { withRouter } from "react-router-dom";
+import { Query } from "react-apollo";
+import { gql } from "apollo-boost";
+
+import Post from "../../../organisms/Post";
+
+export const PostPresenter = props => <Post {...props} />;
+
+const GET_POST = gql`
+  query GetPost($id: ID!) {
+    post(id: $id) {
+      id
+      title
+      content
+    }
+  }
+`;
+
+export const PostContainer = ({ match }) => (
+  <Query query={GET_POST} variables={{ id: match.params.id }}>
+    {({ loading, error, data }) => {
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error...</p>;
+
+      return <PostPresenter post={data.post} />;
+    }}
+  </Query>
+);
+
+export default withRouter(PostContainer);
