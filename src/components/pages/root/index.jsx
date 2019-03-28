d6f566e80d9be56054c2783e62f3abac539d4390
suzukalight <masahiko_kubara@relationsgroup.co.jp>
2019-03-29 00:18:52 +0900

    Use query in root page

--- src/components/pages/root/index.jsx
+++ src/components/pages/root/index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Card, Container } from "semantic-ui-react";
+import { Query } from "react-apollo";
+import { gql } from "apollo-boost";
 
 const Post = ({ post: { id, title, content } }) => (
   <Link to={`/posts/${id}`}>
@@ -24,4 +26,25 @@ export const RootPresenter = ({ posts = [] }) => (
   </Container>
 );
 
-export default RootPresenter;
+const LIST_POSTS = gql`
+  query AllPosts {
+    posts {
+      id
+      title
+      content
+    }
+  }
+`;
+
+export const Root = () => (
+  <Query query={LIST_POSTS}>
+    {({ loading, error, data }) => {
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error...</p>;
+
+      return <RootPresenter posts={data.posts} />;
+    }}
+  </Query>
+);
+
+export default Root;
